6cd7cbb68330588036b301c3856baf7b36de03a2
Cristina Mendoza <cmendo16@email.cpcc.edu>
2024-10-08 10:53:03 -0400

    Set up routing and added app.use to handle routes

--- back-end/app.js
+++ back-end/app.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const routes = require('./routes/routes')
 
-/*app.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
-}); */
-
-app.get('/api/endpoint', (req, res) => {
-    console.log("Endpoint was hit");
-    res.status(200).json({ message: 'Hello from the back-end!' });
 });
+
+
+app.use('/', routes);
 
 module.exports = app;
