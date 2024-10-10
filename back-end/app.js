2efdbd8627837a0a3d1549993be2c112a148d550
Cristina Mendoza <cmendo16@email.cpcc.edu>
2024-10-09 22:06:12 -0400

    fix code for back-end testing

--- back-end/app.js
+++ back-end/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
-const routes = require('./routes/routes')
+const routes = require('./routes/routes');
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
 
+
+app.get('/api/endpoint', (req, res) => {
+    console.log("Endpoint was hit");
+    res.status(200).json({ message: "Hello from the back-end!" });
+})
 
 app.use('/', routes);
 
