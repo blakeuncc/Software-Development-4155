82fd85db20a5f61d70c9cb30d4b14af1c968513a
Cristina Mendoza <cmendo16@email.cpcc.edu>
2024-10-11 10:40:17 -0400

    update routes for the pages

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/Login.jsx'; // Make sure the path is correct to your login component
+import Login from './pages/Login.jsx';
+import Contact from './pages/Contact.jsx';
+import Report from './pages/Report.jsx';
+import Home from './pages/Home.jsx';
+import Map from './pages/Map.jsx';
+import Register from './pages/Register';
 
 const App = () => {
   return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<Navigate to="/Login" />} />
-          <Route path="/Login" element={<Login />} />
-          {/* Add other routes as needed */}
-        </Routes>
-      </Router>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/Home" />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Report" element={<Report />} />
+        <Route path="/Map" element={<Map />} />
+        <Route path="/Register" element={<Register />} />
+      </Routes>
+    </Router>
   );
 };
 
