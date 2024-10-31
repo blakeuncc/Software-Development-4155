067864a3c7993c4ab8954b49b924da8b7a18b60a
Cristina Mendoza <cmendo16@email.cpcc.edu>
2024-10-31 03:50:10 -0400

    Add code to connect to database and update routes

--- back-end/app.js
+++ back-end/app.js
@@ -1,20 +1,29 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes/routes');
-const PORT = process.env.PORT || 3000;
+const userRoutes = require('./routes/userRoutes');
+const PORT = process.env.PORT || 5000;
+const mongoose = require('mongoose');
 
-//app.set('view engine', 'ejs');
+mongoURI = 'mongodb+srv://cmendosalazn22:8UTFOIiJCBoYLTqC@mydb.nji1g.mongodb.net/CLTAlertDB?retryWrites=true&w=majority&appName=MyDB'
+//connect to database
+mongoose.connect(mongoURI)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`);
+        });
+    })
+    .catch(err => console.log(err.message));
 
-// app.listen(PORT, () => {
-//     console.log(`Server is running on http://localhost:${PORT}`);
-// });
-
+app.use(express.json());
 
 app.get('/api/endpoint', (req, res) => {
     console.log("Endpoint was hit");
     res.status(200).json({ message: "Hello from the back-end!" });
 })
 
-app.use('/', routes);
+
+app.use('/users', userRoutes);
+
 
 module.exports = app;
